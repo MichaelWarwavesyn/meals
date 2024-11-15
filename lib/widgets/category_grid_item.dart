8e4bdcba6bd53ba295150f433a508db26aa314f0
Michael Warwavesyn <MichaelWarwavesyn@gmail.com>
2024-11-15 17:23:14 -0500

    Added border radius and made them tappable, currently prints the title.

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -11,18 +11,24 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20.0),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
-        category.color.withOpacity(0.55),
-        category.color.withOpacity(0.9),
-      ])),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onBackground,
-            ),
+    return InkWell(
+      onTap: () => print('tapped: ${category.title}'),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            gradient: LinearGradient(colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ])),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
       ),
     );
   }
