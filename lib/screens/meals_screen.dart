6745d1f87d7b649a47253d43ea871f6dafe97d46
Michael Warwavesyn <MichaelWarwavesyn@gmail.com>
2024-12-19 14:07:46 -0500

    163: Created new meal item widget using card and stack, installed new package for transparent image, Replaced our old card in meals screen with our new meal_item.

--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({required this.title, required this.meals, super.key});
@@ -16,14 +17,7 @@ class MealsScreen extends StatelessWidget {
           : ListView.builder(
               itemCount: meals.length,
               itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  color: (meals[index].isVegan || meals[index].isVegetarian)
-                      ? Colors.green
-                      : Colors.grey,
-                  child: Column(
-                    children: [Text(meals[index].title)],
-                  ),
-                );
+                return MealItem(meal: meals[index]);
               },
             ),
     );
